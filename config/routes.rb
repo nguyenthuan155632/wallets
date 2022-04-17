3312506f5ac1a93d2714215588ce463186774e85
nguyenthuan155632 <nt.apple.it@gmail.com>
2022-04-17 18:59:03 +0700

    sidekiq UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
+  
   get 'prices/index'
   resources :networks
   devise_for :users
