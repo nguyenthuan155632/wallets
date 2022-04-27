9d513fb295f3828fcccd335ce9ad618de5eb2c79
nguyenthuan155632 <nt.apple.it@gmail.com>
2022-04-27 23:25:48 +0700

    Migration error

--- db/migrate/20220427140411_add_number_of_tokens_to_token.rb
+++ db/migrate/20220427140411_add_number_of_tokens_to_token.rb
@@ -2,6 +2,6 @@ class AddNumberOfTokensToToken < ActiveRecord::Migration[6.1]
   def change
     add_column :tokens, :number_of_tokens, :string, default: ''
 
-    RefreshDailyJob.perform_now
+    # RefreshDailyJob.perform_now
   end
 end
