78f62425169b7b4623354eb37ceeba48cc33faa0
nguyenthuan155632 <nt.apple.it@gmail.com>
2022-04-27 23:54:01 +0700

    change from mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'vensera2010@outlook.com'
+  default from: 'nt.apple.it@gmail.com'
   layout 'mailer'
 end
