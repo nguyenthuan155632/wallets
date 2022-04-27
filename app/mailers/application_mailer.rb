8258f622dbf78afa29ccda46475a0d09b23130aa
nguyenthuan155632 <nt.apple.it@gmail.com>
2022-04-27 23:34:03 +0700

    change from mail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'wallet-guys@gmail.com'
+  default from: 'app249698135@heroku.com'
   layout 'mailer'
 end
