502d01879f563ede5d73f4be5618c0de5eadc93f
nguyenthuan155632 <nt.apple.it@gmail.com>
2022-04-30 20:10:53 +0700

    change mail domain.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: ENV['MAILGUN_SMTP_SERVER'],
     port: ENV['MAILGUN_SMTP_PORT'],
-    domain: 'wallet-guys.herokuapp.com',
+    domain: 'thuanwallets.herokuapp.com',
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
     enable_starttls_auto: false
